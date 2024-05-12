2127b423a3f6919b4a6047e0a2d8e939d64150ae
David Zapata <dzr794@gmail.com>
2024-05-12 17:32:01 -0500

    added highlithed games functionality

--- src/config/game_mini_ecommerce.sql
+++ src/config/game_mini_ecommerce.sql
@@ -1,9 +1,10 @@
+drop database games_mini_ecommerce;
 CREATE database games_mini_ecommerce;
 
 use games_mini_ecommerce;
 
 # show tables;
-# drop database games_mini_ecommerce;
+
 
 CREATE TABLE Games (
 	id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -19,6 +20,11 @@ CREATE TABLE Games (
 CREATE INDEX gamePrice_idx
 ON Games(price);
 
+CREATE TABLE HighlightedGames (
+	gameId int PRIMARY KEY NOT NULL,
+    lastUpdatedDate datetime DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
 CREATE TABLE ESRB (
 	id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name varchar(255) NOT NULL,
@@ -26,6 +32,11 @@ CREATE TABLE ESRB (
 );
 
 CREATE TABLE Publishers (
+	id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name varchar(255) NOT NULL
+);
+
+CREATE TABLE Genres (
 	id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name varchar(255) NOT NULL
 );
@@ -62,6 +73,12 @@ CREATE TABLE Game_User_relations (
     PRIMARY KEY (userId, gameId)
 );
 
+CREATE TABLE Game_Genre_relations (
+    genreId int NOT NULL,
+    gameId int NOT NULL,
+    PRIMARY KEY (genreId, gameId)
+);
+
 CREATE TABLE User_Role_relations (
 	userId int NOT NULL,
     roleId int NOT NULL DEFAULT 1,
@@ -80,6 +97,12 @@ ADD CONSTRAINT fk_GamePublisher
     REFERENCES Publishers (id)
     ON DELETE CASCADE;
     
+ALTER TABLE HighlightedGames
+ADD CONSTRAINT fk_GameId
+	FOREIGN KEY (gameID)
+    REFERENCES Games (id)
+    ON DELETE CASCADE;
+    
 ALTER TABLE Orders
 ADD CONSTRAINT fk_OrderUser
     FOREIGN KEY (userId)
@@ -96,6 +119,18 @@ ALTER TABLE Order_Game_relations
 ADD CONSTRAINT fk_OG_Game
     FOREIGN KEY (gameId)
     REFERENCES Games (id)
+    ON DELETE CASCADE;
+    
+ALTER TABLE Game_Genre_relations
+ADD CONSTRAINT fk_GG_Game
+    FOREIGN KEY (gameId)
+    REFERENCES Games (id)
+    ON DELETE CASCADE;
+
+ALTER TABLE Game_Genre_relations
+ADD CONSTRAINT fk_GG_Genre
+    FOREIGN KEY (genreId)
+    REFERENCES Genres (id)
     ON DELETE CASCADE;
 
 ALTER TABLE Game_User_relations
@@ -124,9 +159,20 @@ ADD CONSTRAINT fk_UR_Role
 
 
 # user with role name view
-CREATE VIEW user_with_role as SELECT u.id as userId, u.displayName as userName, r.id as roleId, r.name as roleName FROM User_Role_relations as URr INNER JOIN Users as u on u.id = URr.userId INNER JOIN Roles as r on r.id = URr.roleId;
+CREATE VIEW user_with_role as 
+	SELECT u.id as userId, u.displayName as userName, r.id as roleId, r.name as roleName 
+    FROM User_Role_relations as URr 
+    INNER JOIN Users as u on u.id = URr.userId 
+    INNER JOIN Roles as r on r.id = URr.roleId;
 
-    
+# highlighted games with details
+CREATE VIEW HighlightedGamesDetails as
+	SELECT g.id, g.title, g.price, g.publisherName as publisher, g.ESRB_ratingName as ESRB, imgLink as image, h.lastUpdatedDate
+    FROM  HighlightedGames as h
+    INNER JOIN Games as g on g.id =  h.gameId
+    ORDER BY h.lastUpdatedDate DESC
+    LIMIT 5;
+
 # Data insertion
 
 INSERT INTO ESRB (id, name, description) VALUES
@@ -159,15 +205,22 @@ INSERT INTO Publishers (id, name) VALUES
 (NULL, "Bandai Namco Entertainment"),
 (NULL, "Larian Studios");
 
-INSERT INTO Games (id, title, price, publisherId, publisherName, ESRB_ratingId, ESRB_ratingName) VALUES
-(NULL, "What Remains of Edith Finch", 19.99 , 1, 'Annapurna Interactive', 3, 'Teen'),
-(NULL, "Rocket League", 0 , 2, 'Psyonix', 2, 'Everyone 10+'),
-(NULL, "Apex Legends", 0 , 4, 'Electronic Arts', 3, 'Teen'),
-(NULL, "Monster Hunter World", 29.99 , 1, 'Annapurna Interactive', 3, 'Teen'),
-(NULL, "Helldivers 2", 29.99 , 1, 'Annapurna Interactive', 4, 'Mature 17+'),
-(NULL, "Baldur's Gate 3", 59.99 , 7, 'Larian Studios', 4, 'Mature 17+'),
-(NULL, "Outer Wilds", 24.99 , 1, 'Annapurna Interactive', 2, 'Everyone 10+'),
-(NULL, "Elden Ring", 59.99 , 6, 'Bandai Namco Entertainment', 3, 'Teen');
+INSERT INTO Games (id, title, price, publisherId, publisherName, ESRB_ratingId, ESRB_ratingName, imgLink) VALUES
+(NULL, "What Remains of Edith Finch", 19.99 , 1, 'Annapurna Interactive', 3, 'Teen', 'https://dbxce1spal1df.cloudfront.net/018d937f-00e8-729d-a917-e2f1d5bcc95d/boxart.jpg'),
+(NULL, "Rocket League", 0 , 2, 'Psyonix', 2, 'Everyone 10+', 'https://dbxce1spal1df.cloudfront.net/018d937f-0a42-73de-a3a3-e7a08bc728b2/boxart.jpg'),
+(NULL, "Apex Legends", 0 , 4, 'Electronic Arts', 3, 'Teen', 'https://dbxce1spal1df.cloudfront.net/018d937f-0ddf-7119-9337-aa96d8a36d39/boxart.jpg'),
+(NULL, "Monster Hunter World", 29.99 , 1, 'Annapurna Interactive', 3, 'Teen', 'https://dbxce1spal1df.cloudfront.net/018d937e-fd94-7238-a99b-14457502670c/boxart.jpg'),
+(NULL, "Helldivers 2", 29.99 , 1, 'Annapurna Interactive', 4, 'Mature 17+', 'https://dbxce1spal1df.cloudfront.net/018d937f-6ee4-73f5-858c-2fde3407462f/boxart.jpg'),
+(NULL, "Baldur's Gate 3", 59.99 , 7, 'Larian Studios', 4, 'Mature 17+', 'https://dbxce1spal1df.cloudfront.net/018d9584-24d6-7010-b82b-df1f0b154cc7/boxart.jpg'),
+(NULL, "Outer Wilds", 24.99 , 1, 'Annapurna Interactive', 2, 'Everyone 10+', 'https://dbxce1spal1df.cloudfront.net/018d937f-09dd-729b-88aa-5496f800b3bd/boxart.jpg'),
+(NULL, "Elden Ring", 59.99 , 6, 'Bandai Namco Entertainment', 3, 'Teen', 'https://dbxce1spal1df.cloudfront.net/018d937f-590c-728b-ac35-38bcff85f086/boxart.jpg');
+
+INSERT INTO HighlightedGames (gameId) VALUES
+(1),
+(4),
+(5),
+(7),
+(8);
 
 INSERT INTO Orders (userId, totalPrice) VALUES
 (1, 89.98),
@@ -209,19 +262,25 @@ INSERT INTO User_Role_relations (userId, roleId) VALUES
 (4,2),
 (5,1),
 (6,1);
-	
+
+
 
 # SELECT * FROM ESRB;
 # SELECT * FROM Users;
 # SELECT * FROM Publishers;
 # SELECT * FROM Games;
+SELECT * FROM HighlightedGamesDetails;
+# INSERT INTO HighlightedGames (gameId) VALUES (6);
 # SELECT * FROM Orders;
 # SELECT * FROM order_game_relations;
 # SELECT * FROM game_user_relations;
 # SELECT * FROM User_Role_relations;
-
+# SELECT * from Genres;
 
 # SELECT * FROM user_with_role ORDER BY roleId desc;
+
+# get 5 highlighted games
+
 
 # get all free games
 # SELECT title, price, publisherName as publisher, ESRB_ratingName as ESRB FROM Games where price = 0;
@@ -232,9 +291,12 @@ INSERT INTO User_Role_relations (userId, roleId) VALUES
 # get all games that cost between 20 and 60USD
 # SELECT * FROM Games where price > 20 AND price < 60;
 
+#get all genres from game
+
+
 # get all games from x user
-SELECT u.id as userId, u.displayName, g.id as gameId, g.title as gameTitle FROM game_user_relations as GUr INNER JOIN Games as g on g.id = GUr.gameId INNER JOIN Users as u on u.id = GUr.userId WHERE userId = 2;
+# SELECT u.id as userId, u.displayName, g.id as gameId, g.title as gameTitle FROM game_user_relations as GUr INNER JOIN Games as g on g.id = GUr.gameId INNER JOIN Users as u on u.id = GUr.userId WHERE userId = 2;
 # SELECT g.title, g.publisher, g.ESRB_rating FROM game_user_relations as r INNER JOIN GamesWithInfo as g on g.id = r.gameid WHERE userid = 2;
 
 
-# show indexes from user_with_role;
+# show indexes from games;
