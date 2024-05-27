42dddf10077967527d01125131546a2561b96408
David Zapata <dzr794@gmail.com>
2024-05-27 14:42:48 -0500

    adding filtering handling to the listGames endpoint

--- src/useCases/Games/listGames.ts
+++ src/useCases/Games/listGames.ts
@@ -5,10 +5,28 @@ import { db } from "../../config/mySqlConnection";
 export const listGames = async ( req:Request, res:Response ) => {
   const conn = await db;
 
-
-  const queryString =  `SELECT id, title, price, publisherName as publisher, ESRB_ratingName as ESRB, imgLink as image
-                        FROM games;`;
+  const query_keys_length = Object.keys(req.query).length;
+  let where_query = '';
   
+  if( query_keys_length > 0 ) {
+    const {price, publisher} = req.query;
+
+    where_query = 'WHERE';
+
+    if (price) { where_query += ` price ${ parseInt(String(price)) > 0 ? '<= ' : '= ' }${price}`; }
+    
+    where_query += (query_keys_length > 1) ? ' AND' : '';
+    
+    if (publisher) { where_query += ` publisherId = ${publisher}`; }
+
+  } else {
+    where_query = '';
+  }
+
+  const queryString =  `SELECT id, title, price, publisherName as publisher, ESRB_ratingName as ESRB, imgLink as image FROM games ${where_query};`;
+
+  // res.send(queryString);
+
   try {
     const [queryResult] = await conn.query(queryString);
     console.log(queryResult);
